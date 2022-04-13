d6a18293a68a6d857a942dd0cf0c010ec9462f6a
Paola Reategui <paolareategui@hotmail.com>
2022-04-13 13:07:54 +1000

    Add new text component for error messages

---
+++ app/components/AppErrorText.js
@@ -0,0 +1,20 @@
+//Makes text red and bolded for error messages
+import React from "react";
+
+import { Text, StyleSheet } from "react-native";
+
+import AppColors from "../config/AppColors";
+
+function AppErrorText({ children, style }) {
+  return <Text style={[styles.text, style]}> {children} </Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 12,
+    color: AppColors.errorColor,
+    paddingVertical: 5,
+  },
+});
+
+export default AppErrorText;
