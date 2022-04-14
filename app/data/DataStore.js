91db8c9ccc8c4bd0a2778553d3c671f9c1beffa1
Paola Reategui <paolareategui@hotmail.com>
2022-04-14 22:54:19 +1000

    Update data store

--- app/data/DataStore.js
+++ app/data/DataStore.js
@@ -1,40 +1,74 @@
 export default class DataStore {
   static myInstance = null;
-  userID = "";
+  userid = "";
 
   entries = [
     {
-      userid: 1,
+      userid: "user1",
       imageid: 1,
       title: "My succulent is thriving",
       entry:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu enim sodales, venenatis libero sed, aliquet erat. Ut commodo mi sem, eu ullamcorper neque fermentum ut. Pellentesque ullamcorper laoreet ipsum, vel ultrices risus. Mauris eu.",
       category: "Succulents",
-      image: require("../app/assets/succulents1.jpeg"),
+      image: require("../assets/succulents1.jpeg"),
     },
     {
-      userid: 1,
+      userid: "user1",
       imageid: 2,
       title: "Month 3 of nursing it back to life",
       entry:
         "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
       category: "Succulents",
-      image: require("../app/assets/foliage.jpeg"),
+      image: require("../assets/foliage1.jpeg"),
+    },
+    {
+      userid: "user2",
+      imageid: 3,
+      title: "Look at this baby grow",
+      entry:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu enim sodales, venenatis libero sed, aliquet erat.",
+      category: "Cactus",
+      image: require("../assets/cactus1.jpeg"),
     },
   ];
+
+  static getInstance() {
+    if (DataStore.myInstance == null) {
+      DataStore.myInstance = new DataStore();
+    }
+    return this.myInstance;
+  }
+
+  getUserID() {
+    return this.userid;
+  }
+
+  setUserID(id) {
+    this.userid = id;
+  }
+
+  getEntries(id) {
+    return this.entries.filter((entry) => entry.userid === id);
+  }
+
+  addEntry(entry) {
+    this.books.push(entry);
+  }
 }
 
-// users = [
-//     {
-//         userid: 1,
-//         username: "Carmen",
-//         useremail: "carmen@gmail.com",
-//         password: "1234",
-//         profileimage: require("../assets/user1.jpeg"),
-//         {
-//             images: {
-
-//             }
-//         }
-//     }
-// ]
+// const users = [
+//   {
+//     id: "user1",
+//     username: "Carmen Carrera",
+//     email: "carmen@mail.com",
+//     password: "1234",
+//     image: require("../app/assets/user1.png"),
+//   },
+//   {
+//     id: "user2",
+//     username: "Tom Craig",
+//     email: "tc@mail.com",
+//     password: "1234",
+//     image: require("../app/assets/user2.png"),
+//   },
+// ];
