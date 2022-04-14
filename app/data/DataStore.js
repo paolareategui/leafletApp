5ed2e3739fa0864f52a03a35be0c6563e2e2a70d
Paola Reategui <paolareategui@hotmail.com>
2022-04-15 00:05:06 +1000

    Add more data

--- app/data/DataStore.js
+++ app/data/DataStore.js
@@ -3,6 +3,7 @@ export default class DataStore {
   userid = "";
 
   entries = [
+    //user1 entries
     {
       userid: "user1",
       imageid: 1,
@@ -21,14 +22,106 @@ export default class DataStore {
       category: "Succulents",
       image: require("../assets/foliage1.jpeg"),
     },
+
     {
-      userid: "user2",
+      userid: "user1",
       imageid: 3,
-      title: "Look at this baby grow",
+      title: "Happy and vibrant",
       entry:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu enim sodales, venenatis libero sed, aliquet erat.",
+        "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
       category: "Cactus",
       image: require("../assets/cactus1.jpeg"),
+    },
+
+    {
+      userid: "user1",
+      imageid: 4,
+      title: "In bloom! ",
+      entry:
+        "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
+      category: "Flowers",
+      image: require("../assets/flowers1.jpeg"),
+    },
+
+    {
+      userid: "user1",
+      imageid: 5,
+      title: "Oh boy, here we go again :(",
+      entry:
+        "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
+      category: "Herbs",
+      image: require("../assets/herbs1.jpeg"),
+    },
+
+    {
+      userid: "user1",
+      imageid: 6,
+      title: "My happy place",
+      entry:
+        "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
+      category: "Foliage2",
+      image: require("../assets/foliage2.jpeg"),
+    },
+
+    {
+      userid: "user1",
+      imageid: 7,
+      title: "She keeps growing more beautiful",
+      entry:
+        "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
+      category: "Hanging",
+      image: require("../assets/hanging1.jpeg"),
+    },
+
+    {
+      userid: "user1",
+      imageid: 8,
+      title: "Prickly little thing",
+      entry:
+        "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
+      category: "Cactus",
+      image: require("../assets/cactus2.jpeg"),
+    },
+
+    {
+      userid: "user1",
+      imageid: 9,
+      title: "These babies came home today",
+      entry:
+        "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
+      category: "Succulents",
+      image: require("../assets/succulents2.jpeg"),
+    },
+
+    //user2 entries
+
+    {
+      userid: "user2",
+      imageid: 10,
+      title: "She keeps on growing",
+      entry:
+        "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
+      category: "Flowers",
+      image: require("../assets/flowers2.jpeg"),
+    },
+
+    {
+      userid: "user2",
+      imageid: 11,
+      title: "Look at her grow. I'm in awe",
+      entry:
+        "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
+      category: "Flowers",
+      image: require("../assets/hanging2.jpeg"),
+    },
+    {
+      userid: "user2",
+      imageid: 13,
+      title: "The smell is amazing",
+      entry:
+        "Integer ut urna urna. Vestibulum hendrerit auctor dui, non ultricies nulla feugiat laoreet. Proin venenatis nibh vel quam gravida, a tristique erat commodo. Nam varius sapien non mi hendrerit consectetur at et odio. Duis vel justo quis dui.",
+      category: "Flowers",
+      image: require("../assets/herbs2.jpeg"),
     },
   ];
 
